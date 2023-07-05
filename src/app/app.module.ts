269a33ca715f87e4c0e5516f85d9d94901664659
Prabhpreet <prabhpreet429@gmail.com>
2023-07-05 15:26:18 -0700

    fix responsiveness and carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavbarComponent,
-      HomeComponent
    ],
    imports: [
       BrowserModule,
